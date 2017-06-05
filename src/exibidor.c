a082c28f172a533b9e03e411adcadfb5fe312f56
Gabriel Naves da Silva <gabri.navess@gmail.com>
2017-06-04 21:01:54 -0300

    Reading and printing some more ClassFile parameters

--- src/exibidor.c
+++ src/exibidor.c
@@ -2,4 +2,7 @@
 
 void showClassFile(FILE* stream, ClassFile class_file) {
     fprintf(stream, "Magic number: %#010x\n", class_file.magic);
+    fprintf(stream, "Minor version: %u\n", class_file.minor_version);
+    fprintf(stream, "Major version: %u\n", class_file.major_version);
+    fprintf(stream, "Constant pool count: %u\n", class_file.constant_pool_count);
 }
--- src/leitor.c
+++ src/leitor.c
@@ -6,17 +6,29 @@ static uint8_t readByte(FILE* fp) {
     return byte;
 }
 
-static void readMagicNumber(ClassFile* class_file, FILE* fp_class_file) {
-    class_file->magic = readByte(fp_class_file);
-    class_file->magic = (class_file->magic << 8) | readByte(fp_class_file);
-    class_file->magic = (class_file->magic << 8) | readByte(fp_class_file);
-    class_file->magic = (class_file->magic << 8) | readByte(fp_class_file);
+static uint16_t read2Byte(FILE* fp) {
+    uint16_t byte2;
+    byte2 = readByte(fp);
+    byte2 = (byte2 << 8) | readByte(fp);
+    return byte2;
+}
+
+static uint32_t read4Byte(FILE* fp) {
+    uint32_t byte4;
+    byte4 = read2Byte(fp);
+    byte4 = (byte4 << 16) | read2Byte(fp);
+    return byte4;
 }
 
 ClassFile readClassFile(char* file_name) {
     FILE *fp_class_file;
     ClassFile class_file;
     fp_class_file = fopen(file_name, "rb");
-    readMagicNumber(&class_file, fp_class_file);
+
+    class_file.magic = read4Byte(fp_class_file);
+    class_file.minor_version = read2Byte(fp_class_file);
+    class_file.major_version = read2Byte(fp_class_file);
+    class_file.constant_pool_count = read2Byte(fp_class_file);
+    /* Read constant pool */
     return class_file;
 }
