32df34363aa6e4fc932bd758a84039a1669f8710
Gabriel Naves da Silva <gabri.navess@gmail.com>
2017-07-01 18:04:37 -0300

    Started implementing frames

---
+++ src/frame.h
@@ -0,0 +1,28 @@
+#ifndef FRAME_H_
+#define FRAME_H_
+
+struct OperandInfo {
+    uint32_t data;
+    struct OperandInfo *next;
+};
+typedef struct OperandInfo OperandInfo;
+
+struct Frame {
+    MethodInfo* methods;
+    uint32_t pc;
+    OperandInfo* operand_stack;
+    uint32_t local_variable_count;
+    uint32_t* local_variables;
+    uint16_t constant_pool_count;
+    CP_table* constant_pool;
+
+    /*
+    elementoListaArrays *listaArrays;
+    elementoListaObjetos *listaObjetos;
+    */
+
+    uint32_t return_address;
+};
+typedef struct Frame Frame;
+
+#endif /* FRAME_H_ */
--- src/leitor_exibidor.c
+++ src/leitor_exibidor.c
@@ -2,12 +2,23 @@
 #include "leitor.h"
 #include "exibidor.h"
 
+#define __EXIBIDOR__
+// #define __JVM__
+
 int main(int argc, char* argv[]) {
     ClassFile hello_world_class;
     checkArgumentAmount(argc);
     checkFileExtension(argv[1]);
     int output_to_terminal = checkOutputOption(argv[2]);
     hello_world_class = readClassFile(argv[1]);
+
+#ifdef __EXIBIDOR__
     showClassFile(output_to_terminal ? stdout : fopen("output.txt", "w+"), &hello_world_class);
+#endif
+
+#ifdef __JVM__
+
+#endif
+
     return 0;
 }
