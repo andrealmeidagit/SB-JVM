bc86574f987b2cd3b403701bce0c29cde152e28d
tutubarao <andre.almeida3@gmail.com>
2017-07-03 12:06:46 -0300

    incompleto/nao testado

--- src/instruction.c
+++ src/instruction.c
@@ -994,6 +994,11 @@ void instruction_invokevirtual(Frame* frame) {
 
     no caso do helloworld - pega a string empilhada e mostra na saída padrão*/
 
+
+/*****************************************
+**** method descreiptor reader function***
+*****************************************/
+
     char* method_descriptor;
     char* parameter_descriptor;
     char* return_descriptor;
@@ -1025,12 +1030,116 @@ void instruction_invokevirtual(Frame* frame) {
     printf("%s\n", parameter_descriptor);
     printf("%s\n", return_descriptor);
 
+/*********************************************/
 
 
-    //printf("%s\n", parameter_descriptor);
+
 
     frame->pc += 3;
 }
+
+union FieldType{
+    int8_t    byte_type;
+    char      char_type;
+    double    double_type;
+    float     float_type;
+    int       integer_type;
+    long int  long_type;
+    char*     class_name_ref;
+    short int short_type;
+    int8_t    bool_type;
+    char*  array_ref; /*?*/
+}FT;
+
+union FieldType* read_field_type (char* str){
+    union FieldType * FT;
+    int j=1;
+    int i=0;
+    if (str[0]=='B')
+        i = 1;
+    if (str[0]=='C')
+        i = 2;
+    if (str[0]=='D')
+        i = 3;
+    if (str[0]=='F')
+        i = 4;
+    if (str[0]=='I')
+        i = 5;
+    if (str[0]=='J')
+        i = 6;
+    if (str[0]=='L')
+        i = 7;
+    if (str[0]=='S')
+        i = 8;
+    if (str[0]=='Z')
+        i = 9;
+    if (str[0]=='[')
+        i = 10;
+
+    switch (i) {
+        case 0:
+            printf("FIELD TIPE DOES NOT EXIST!!! Base Type Character:%c\n", str[0]);
+            exit(EXIT_FAILURE);
+            break;
+        case 1:
+            printf("implementar field_type->byte\n" );
+            break;
+        case 2:
+            printf("implementar field_type->char\n" );
+            break;
+        case 3:
+            printf("implementar field_type->double\n" );
+            break;
+        case 4:
+            printf("implementar field_type->float\n" );
+            break;
+        case 5:
+            printf("implementar field_type->int\n" );
+            break;
+        case 6:
+            printf("implementar field_type->long\n" );
+            break;
+        case 7:
+            FT->class_name_ref = (char*) malloc (strlen(str)-2);
+            if (FT->class_name_ref==NULL) exit (1);
+            while (str[j]!=';'){
+                FT->class_name_ref[j-1] = str[j];
+                j++;
+            }
+            FT->class_name_ref[j]='\0';
+            printf("%s\n", FT->class_name_ref);
+            break;
+        case 8:
+            printf("implementar field_type->short\n" );
+            break;
+        case 9:
+            printf("implementar field_type->bool\n" );
+            break;
+        case 10:
+            printf("implementar field_type->array\n" );
+            break;
+    }
+    return FT;
+}
+
+/*
+struct MethodDescriptor{
+    char* parameter_descriptor;
+    char* return_descriptor;
+}Meth_Desc;
+typedef struct MethodDescriptor MethodDescriptor;
+
+MethodDescriptor * read_method_descriptor (Frame* frame, uint16_t index){
+MethodDescriptor Meth_Desc;
+
+
+  return Meth_Desc;
+}
+
+*/
+
+
+
 
 void instruction_invokespecial(Frame* frame) {
 
