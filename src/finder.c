b84176644e91fe60cdf391bcdf2b15ffbdb0fd5f
Gabriel Naves da Silva <gabri.navess@gmail.com>
2017-07-04 04:09:59 -0300

    Implemented a function that finds a specific class file in array by name

--- src/finder.c
+++ src/finder.c
@@ -24,3 +24,15 @@ AttributeInfo* findCodeAttribute(const MethodInfo* method, const ConstantInfo* c
     exit(EXIT_FAILURE);
     return NULL;
 }
+
+ClassFile* findClassFile(char* class_name, ClassFile* class_files, uint16_t class_files_count) {
+    for (int i = 0; i < class_files_count; ++i) {
+        ClassFile* class_file = class_files + i;
+        char* class_file_name = (char*)class_file->constant_pool[class_file->constant_pool[class_file->this_class-1].CONSTANT.Class_info.name_index-1].CONSTANT.Utf8_info.bytes;
+        printf("class file name: %s\n", class_file_name);
+        if (strcmp(class_file_name, class_name) == 0)
+            return class_file;
+    }
+    fprintf(stderr, "[ERROR]: ClassFileNotFoundError: %s\n", class_name);
+    return NULL;
+}
