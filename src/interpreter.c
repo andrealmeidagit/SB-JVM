4a2b6217318a1b4ce370a5749cf99580f2ebe6f6
Gabriel Naves da Silva <gabri.navess@gmail.com>
2017-07-03 21:10:09 -0300

    Removing magic number from interpreter.c

--- src/interpreter.c
+++ src/interpreter.c
@@ -1,12 +1,11 @@
 #include "interpreter.h"
 
+static uint8_t return_instr_opcode = 0xb1;
+
 void runFrame(Frame* frame) {
-    uint8_t current_opcode;
-    current_opcode = frame->method_info->attributes[0].u.Code.code[frame->pc];
+    uint8_t current_opcode = frame->method_info->attributes[0].u.Code.code[frame->pc];
     printf("**************\nCurrent opcode: %u\n", current_opcode);
     INSTRUCTION_ARRAY[current_opcode](frame);
-    if (current_opcode != 0xb1)
+    if (current_opcode != return_instr_opcode)
         runFrame(frame);
-    if (frame->previous != NULL)
-        runFrame(frame->previous);
 }
