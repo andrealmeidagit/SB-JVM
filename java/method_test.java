b7adef30f68e7e81471a0f65fb7424ac2a15ab92
Gabriel Naves da Silva <gabri.navess@gmail.com>
2017-07-04 02:55:03 -0300

    Updated method_test java files

--- java/method_test.java
+++ java/method_test.java
@@ -6,27 +6,22 @@
  *  -400000000
  */
 
-
 public class method_test {
-  public static void main(String args[]){
-	  System.out.println(Soma(2,3));
+    public static void main(String args[]){
+        System.out.println(Soma(2,3));
+        System.out.println(Soma(35.35,30.35));
+        System.out.println(Soma((long)300000000,(long)700000000));
+    }
 
-	  System.out.println(Soma(35.35,30.35));
-	  System.out.println(Soma((long)300000000,(long)700000000));
-  }
-  public static int Soma(int x, int y) {
-    return x+y;
-  } 
-  
+    public static int Soma(int x, int y) {
+        return x+y;
+    }
 
-  
-  public static double Soma(double x, double y) {
-    return x+y;
-  } 
-  
-  public static long Soma(long x, long y) {
-    return x-y;
-  } 
-  
+    public static double Soma(double x, double y) {
+        return x+y;
+    }
 
+    public static long Soma(long x, long y) {
+        return x-y;
+    }
 }
