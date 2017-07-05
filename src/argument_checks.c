62432a8129b1a8aa51e217c1cad71d6660044ffa
ricardo <japito238@gmail.com>
2017-07-05 03:26:12 -0300

    D A A C C C C

--- src/argument_checks.c
+++ src/argument_checks.c
@@ -1,18 +1,30 @@
+/** @brief Inclusão da biblioteca de verificação de argumentos */
+ 
 #include "argument_checks.h"
 
+/**  Instrução de uso correto do programa e escolha do tipo de output */
 static void showUsage() {
     printf("Uso: leitor_exibidor.exe nome_da_classe.class <opcao de output>\n");
     printf("<opcao de output> = '-a' ou '--arquivo' ou '-t' ou '--terminal'\n");
 }
 
+/** Checagem do número de parametros
+    @param[in] argc contador de argumentos.
+       
+*/
 void checkArgumentAmount(int argc) {
     if (argc != 3) {
+        /** Erro caso o número de parametros seja diferente de 3 */
         fprintf(stderr, "[ERRO]: Quantidade invalida de argumentos.\n");
         showUsage();
         exit(1);
     }
 }
 
+/** Checagem da extensão do arquivo. 
+    @param[in] file_name Ponteiro do tipo char que aponta para o começo do arquivo.
+
+*/
 void checkFileExtension(char* file_name) {
     if (strstr(file_name, ".class") == NULL) {
         fprintf(stderr, "[ERRO]: O arquivo deve ter a extensao .class\n");
@@ -21,6 +33,9 @@ void checkFileExtension(char* file_name) {
     }
 }
 
+/** Escolha do tipo de Output definida pelo usuário. 
+    @param[in] op Paramentro de escolha de tipo de Output: Terminal ou Arquivo.
+*/
 int checkOutputOption(char* op) {
     if (strcmp(op, "-a") == 0 || strcmp(op, "--arquivo") == 0)
         return 0;
--- src/main.c
+++ src/main.c
@@ -1,3 +1,25 @@
+/*
+Universidade de Brasília - 01/2017
+Software Básico - Turma A
+Projeto Java Virtual Machine
+
+Alunos: 		Gabriel Naves da Silva 				12/0011867
+				Fernando Lucchesi Alencar 			11/0060571
+				Filipe Pereira Fortes 				09/0113799
+				André Abreu Rodrigues de Almeida	12/0007100
+				Ricardo Hideki Ito 					12/0045818
+*/
+
+/**
+ * @file main.c
+ * @brief Execução do programa pela função 'main'. 
+ */
+
+/** Inclusão de bibliotecas para checagem de argumentos, leitura do arquivo .class em bytecode, exibição dos 
+campos do bytecode, criação de frames, implementação de instruções.
+*/
+
+
 #include "argument_checks.h"
 #include "leitor.h"
 #include "exibidor.h"
@@ -9,16 +31,35 @@
 // #define __EXIBIDOR__
 #define __JVM__
 
+
+/**
+ * 
+ * @brief Função main
+ * 
+ * @param argc Número de argumentos do programa
+ * @param argv Argumentos de linha de comando
+ * 
+ */
 int main(int argc, char* argv[]) {
+
+	/** Verificação do arquivo .class. Checa quantidade de parametros, extensão do arquivo. 	*/
+
     ClassFile input_class;
     checkArgumentAmount(argc);
     checkFileExtension(argv[1]);
+
+    /** Recebe o .class file */
     input_class = readClassFile(argv[1]);
+
+
+    /** Exibição do arquivo .class se o código EXIBIDOR estiver definido. */
 
 #ifdef __EXIBIDOR__
     int output_to_terminal = checkOutputOption(argv[2]);
     showClassFile(output_to_terminal ? stdout : fopen("output.txt", "w+"), &input_class);
 #endif
+
+    /** @brief Busca, Execução do Método e Criação de Frames recursivamente. */
 
 #ifdef __JVM__
     FRAME_AMOUNT = 0;
